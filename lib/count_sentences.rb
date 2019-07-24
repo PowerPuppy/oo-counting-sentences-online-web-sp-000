faf125e2f6707f6945909ad64900c17cd0f0033b
Chris Cheong <chrisxcheong@gmail.com>
2019-07-24 05:36:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,9 +17,9 @@ class String
   def count_sentences
     count = 0
     delimiters = ['.', '?', "!"]
-self.split(delimiters).each {|sentence| if sentence.sentence? == true count +=1
-if sentence.question? == true count +=1
-if sentence.exclamation? == true count +=1
+self.split(delimiters).each {|sentence| if sentence.sentence? == true, count +=1
+if sentence.question? == true, count +=1
+if sentence.exclamation? == true, count +=1
 end}
 end
   return count
